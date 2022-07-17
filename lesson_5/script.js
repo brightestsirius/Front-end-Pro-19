98618c664bf79c93f87fad06db118165553eaeb9
brightestsirius <katherina.shypovskaya@gmail.com>
2022-07-17 11:14:46 +0300

    lesson_5 – todo

---
+++ lesson_5/script.js
@@ -0,0 +1,19 @@
+// while
+// do/while
+// break; continue;
+// for
+// Array
+// Array methods
+    // push
+    // pop
+    // shift
+    // unshift
+    // reverse
+    // concat
+    // slice
+    // splice
+    // join
+    // split
+    // indexOf
+// Function
+// Lexical environment
