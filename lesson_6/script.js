11525f94f47cb7c51985f51eaa1e7d787be6fa62
brightestsirius <katherina.shypovskaya@gmail.com>
2022-07-20 15:28:43 +0300

    lesson_6 – todo

---
+++ lesson_6/script.js
@@ -0,0 +1,16 @@
+// Array
+// Array methods
+    // push
+    // pop
+    // shift
+    // unshift
+    // reverse
+    // concat
+    // slice
+    // splice
+    // join
+    // split
+    // indexOf
+// Function
+// Lexical environment
+// Scope
