42604833539fe9ab5b424dbbdb541055e03d910c
brightestsirius <katherina.shypovskaya@gmail.com>
2022-07-24 10:20:47 +0300

    lesson_7 – todo

--- lesson_7/script.js
+++ lesson_7/script.js
@@ -1,9 +1,33 @@
 // document.write()
 
 // Factorial
+// Factorial of number
+// Sum of factorials 1-10
 
-// Fibonacci
+// Fibonacci numbers
+// Print 10 numbers if Fibonacci
 
-// Pure numbers
+// Prime number
+// %
 
 // Array
+// length
+
+// min/max element in array
+// min/max element in array by index
+// change position of min/max
+
+// Math.random()
+
+// Array methods
+//     push
+//     pop
+//     shift
+//     unshift
+//     reverse
+//     concat
+//     slice
+//     splice
+//     join
+//     split
+//     indexOf
