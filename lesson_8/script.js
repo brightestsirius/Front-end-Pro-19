8a209a0fcf03b370381d1439cf7cda3495b739ea
brightestsirius <katherina.shypovskaya@gmail.com>
2022-07-27 14:31:22 +0300

    lesson_8 – todo

---
+++ lesson_8/script.js
@@ -0,0 +1,26 @@
+// min/max element in array by index
+// change position of min/max
+
+// Copy of array
+
+// Array methods
+//     push
+//     pop
+//     shift
+//     unshift
+//     join
+
+// Chess board
+
+//     reverse
+//     concat
+//     slice
+//     splice
+//     split
+//     indexOf
+
+// Two dimensional array
+
+// isArray();
+
+// Function
