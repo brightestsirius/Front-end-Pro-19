77e1e102789c8019d5b75a9b2a69663903f423df
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-07 10:24:18 +0300

    lesson_1 – todo

---
+++ lesson_11/script.js
@@ -0,0 +1,16 @@
+// push element to array
+// calculator
+
+// forEach()
+// filter()
+// map()
+// find()/findIndex()
+// every()/some()
+// reduce()
+
+// Object
+
+// Object.keys
+// Object.entries
+
+// Object prototype
