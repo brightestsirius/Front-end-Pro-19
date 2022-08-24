fdda5468f4a3480da99573630954fc82641e2fca
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-24 19:11:06 +0300

    lesson_16 – todo

---
+++ lesson_16/script.js
@@ -0,0 +1,14 @@
+// Static methods
+// get/set
+// this in arrow function
+// call/apply/bind
+// Object.defineProperty()
+
+// Destructuring assignment
+
+// Приватні та захищені властивості та методи
+// Міксини
+// Патерни проектування
+// Typescript
+
+// DOM
