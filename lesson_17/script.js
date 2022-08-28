4cdea8c563c467000e0674912069f9a188384d1a
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-28 10:45:53 +0300

    lesson_17 – todo

---
+++ lesson_17/script.js
@@ -0,0 +1,49 @@
+// Object.getOwnPropertyDescriptor(obj, prop)
+
+let user = {}
+
+Object.defineProperty(user, `name`, {enumerable: true});
+console.log(Object.getOwnPropertyDescriptor(user, `name`));
+
+// Object.defineProperty (get, set)
+// Destructuring assignment
+
+// DOM
+// document
+// redirect
+// querySelector
+// html
+// css
+// class
+// attributes
+// dataset
+// createElement
+// remove
+// events
+
+// click events
+//     onclick
+//     ondblclick
+//     oncontextmenu
+//     onmousedown
+//     onmouseup
+
+// addEventListener/removeEventListener
+
+// mouse events
+//     onmouseover
+//     onmouseout
+//     onmouseenter
+//     onmouseleave
+//     onmousemove
+
+// keyboard events
+//     onkeypress
+//     onkeydown
+//     onkeyup
+
+// Bubbling and capturing
+
+// stopPropagation
+
+// preventDeafault()
