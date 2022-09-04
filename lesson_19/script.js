0c2cf4b2b717f74d2bfdb6e6d654dd9867ecfd82
brightestsirius <katherina.shypovskaya@gmail.com>
2022-09-04 10:56:17 +0300

    lesson_19 – todo

---
+++ lesson_19/script.js
@@ -0,0 +1,34 @@
+// events
+
+// click events
+//     onclick
+//     ondblclick
+//     oncontextmenu
+//     onmousedown
+//     onmouseup
+
+// addEventListener/removeEventListener
+
+// mouse events
+//     onmouseover
+//     onmouseout
+//     onmouseenter
+//     onmouseleave
+//     onmousemove
+
+// Drag & Drop practice
+
+// keyboard events
+//     onkeypress
+//     onkeydown
+//     onkeyup
+
+// Bubbling and capturing
+
+// stopPropagation
+
+// preventDeafault()
+
+// localStorage/sessionStorage
+
+// Calendar practice
